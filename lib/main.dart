1936e745dcfb52ec482a4151d412f7e5ed5f2aed
Yohannes Masterous <yohannes.masterous@gmail.com>
2025-05-03 09:38:12 +0700

    Add Splash Screen and Icon App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:fasum/firebase_options.dart';
-import 'package:fasum/screens/home_screen.dart';
-import 'package:fasum/screens/sign_in_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fasum/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -22,19 +20,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fasum',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          } else {
-            return const SignInScreen();
-          }
-        },
-      ),
+      home: SplashScreen(),
     );
   }
 }
